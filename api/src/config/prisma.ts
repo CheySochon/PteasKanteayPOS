d97ed0baf607b978a48c3b1ab21ccd055a19d5bd
Cheysochon <Cheychon258@gmail.com>
2026-09-09 19:23:37 +0700

    Use datasources option in PrismaClient for TypeScript types compatibility

--- api/src/config/prisma.ts
+++ api/src/config/prisma.ts
@@ -2,7 +2,11 @@ import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
 
 export { prisma };
